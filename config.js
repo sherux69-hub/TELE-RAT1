f0e7b3888baf6d2f7261b1036f3fe8efc02faa37
sherux69-hub <sherux69@gmail.com>
2025-10-02 19:43:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const parsedId = Number(process.env.ID);
 module.exports = {
-  token: process.env.TOKEN || 'YOUR_BOT_TOKEN_HERE',
-  id: isNaN(parsedId) ? 123456789 : parsedId // replace 12345.. with your telegram chat id
+  token: process.env.TOKEN || '7552058896:AAFG_9zn1WO4HE2BshHPLb65aIdjPYNrLRU',
+  id: isNaN(parsedId) ? 8394167087 : parsedId // replace 12345.. with your telegram chat id
 };
